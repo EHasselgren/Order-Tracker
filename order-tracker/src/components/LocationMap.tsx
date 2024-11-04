b063560d2961b71314b4bea48278d4c11f697c88
Elias Hasselgren <Elias.hasselgren@icloud.com>
2024-11-04 17:35:17 +0100

    Fixing issues

--- order-tracker/src/components/LocationMap.tsx
+++ order-tracker/src/components/LocationMap.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { Icon } from 'leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const customIcon = new Icon({
-  iconUrl: '/marker-icon.png',
-  shadowUrl: '/marker-shadow.png',
+const ICON_URL = '/marker-icon.png';
+const SHADOW_URL = '/marker-shadow.png';
+
+const defaultIcon = L.icon({
+  iconUrl: ICON_URL,
+  shadowUrl: SHADOW_URL,
   iconSize: [25, 41],
-  iconAnchor: [12, 41]
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
 });
+
+L.Marker.prototype.options.icon = defaultIcon;
 
 interface LocationMapProps {
   latitude: number;
@@ -17,11 +24,26 @@ interface LocationMapProps {
 }
 
 const LocationMap: React.FC<LocationMapProps> = ({ latitude, longitude, locationName }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+    }, 100);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (!latitude || !longitude) {
+    return <div className="rounded-lg overflow-hidden border border-gray-200 h-[200px] bg-gray-100 flex items-center justify-center">
+      <p className="text-gray-500">Location coordinates not available</p>
+    </div>;
+  }
+
   return (
     <div className="rounded-lg overflow-hidden border border-gray-200">
-      <MapContainer 
-        center={[latitude, longitude]} 
-        zoom={13} 
+      <MapContainer
+        key={`${latitude}-${longitude}`}
+        center={[latitude, longitude]}
+        zoom={13}
         style={{ height: '200px', width: '100%' }}
         scrollWheelZoom={false}
       >
@@ -29,7 +51,7 @@ const LocationMap: React.FC<LocationMapProps> = ({ latitude, longitude, location
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={[latitude, longitude]} icon={customIcon}>
+        <Marker position={[latitude, longitude]}>
           <Popup>
             <div className="text-sm">
               <strong>{locationName}</strong><br />
